c823993316ae4268138ba2ac62afbac9c1e29de6
jdruk <desenvolvedorjosafa@gmail.com>
2016-02-03 20:46:36 -0300

    function user controler view configured

--- app/controllers/function_user_projects_controller.rb
+++ app/controllers/function_user_projects_controller.rb
@@ -1,10 +1,13 @@
 class FunctionUserProjectsController < ApplicationController
   before_action :set_function_user_project, only: [:show, :edit, :update, :destroy]
+  before_filter do
+    redirect_to no_project_selected_path unless has_project_selected?
+  end
 
   # GET /function_user_projects
   # GET /function_user_projects.json
   def index
-    @function_user_projects = FunctionUserProject.all
+    @function_user_projects = FunctionUserProject.where(project_id: current_project_id)
   end
 
   # GET /function_user_projects/1
@@ -25,7 +28,7 @@ class FunctionUserProjectsController < ApplicationController
   # POST /function_user_projects.json
   def create
     @function_user_project = FunctionUserProject.new(function_user_project_params)
-
+    @function_user_project.project_id = current_project_id
     respond_to do |format|
       if @function_user_project.save
         format.html { redirect_to @function_user_project, notice: 'Function user project was successfully created.' }
@@ -64,11 +67,11 @@ class FunctionUserProjectsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_function_user_project
-      @function_user_project = FunctionUserProject.find(params[:id])
+      @function_user_project = FunctionUserProject.where(id: params[:id], project_id: current_project_id).first
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def function_user_project_params
-      params.require(:function_user_project).permit(:function_id, :user_id, :project_id)
+      params.require(:function_user_project).permit(:function_id, :user_id)
     end
 end
