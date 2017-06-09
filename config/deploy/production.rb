03faa82d3393eabaa6b1c60e9e1e15d9c325949b
josafa <desenvolvedorjosafa@gmail.com>
2017-06-09 15:17:20 -0500

    fixed capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,8 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-
-
+server '192.241.131.164', user: 'deploy', roles: %w{app db web}
 
 # role-based syntax
 # ==================
