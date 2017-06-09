978219532cc297952a9479f8871d666c2b9c8afa
josafa <desenvolvedorjosafa@gmail.com>
2017-06-09 16:39:55 -0500

    seed

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,3 +9,14 @@ append :linked_files, "config/database.yml", "config/secrets.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
 
 set :keep_releases, 2
+
+desc 'Runs rake db:seed'
+task :seed => [:set_rails_env] do
+  on primary fetch(:migration_role) do
+    within release_path do
+      with rails_env: fetch(:rails_env) do
+        execute :rake, "db:seed"
+      end
+    end
+  end
+end
